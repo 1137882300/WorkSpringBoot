045d50d5092814cc386f667bc4cacdbb755c62ee
zhongzihan <1137882300@qq.com>
2022-06-19 12:11:16 +0800

    feature: yml

---
+++ spring-boot/src/main/java/com/zhong/springboot/jackson/parseYml.java
@@ -0,0 +1,36 @@
+package com.zhong.springboot.jackson;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.zhong.springboot.snakeyaml.YmlConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+/**
+ * Created by cc on 2022/6/19
+ */
+public class parseYml {
+
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        InputStream resourceAsStream =
+                parseYml.class.getClassLoader().getResourceAsStream("parse.yml");
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+        assert resourceAsStream != null;
+        Map map = mapper.readValue(resourceAsStream, Map.class);
+        Map<String, Object> oo = (Map) ((Map) map.get("meta")).get("oss");
+
+        YmlConfig ymlConfig = new YmlConfig();
+        ymlConfig.setUrl((String) oo.get("url"));
+        ymlConfig.setPassword((String) oo.get("password"));
+        ymlConfig.setUsername((String) oo.get("username"));
+
+        System.out.println(ymlConfig);
+
+    }
+
+}
