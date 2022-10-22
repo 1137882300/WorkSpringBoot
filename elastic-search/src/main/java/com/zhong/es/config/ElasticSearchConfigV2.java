9aeb77dd0547fd1aa0c19c3505ea35eebf5575b4
923828430@qq.com <zhong1137882300>
2022-10-22 11:31:13 +0800

    es

---
+++ elastic-search/src/main/java/com/zhong/es/config/ElasticSearchConfigV2.java
@@ -0,0 +1,8 @@
+package com.zhong.es.config;
+
+public class ElasticSearchConfigV2
+//        extends AbstractElasticsearchConfiguration
+{
+//<!--      <artifactId>spring-boot-starter-data-elasticsearch</artifactId>-->
+
+}
