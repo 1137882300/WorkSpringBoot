05cf2dfeccb8adb7556939c5d2312118c63a1eca
zzh <zzh@youxiake.com>
2023-04-07 14:23:19 +0800

    压测包

--- spring-boot/src/main/java/com/zhong/Application.java
+++ spring-boot/src/main/java/com/zhong/Application.java
@@ -2,14 +2,14 @@ package com.zhong;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @Slf4j
 //@MapperScan("com.zhong.repository.mapper")
 //忽略mysql ,exclude={DataSourceAutoConfiguration.class}
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
