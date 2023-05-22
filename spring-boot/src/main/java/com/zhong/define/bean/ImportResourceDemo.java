bde055c39f2d86d6b891fc4b5551340bec8ba0a7
zzh <zzh@youxiake.com>
2023-05-22 11:25:41 +0800

    2023-5-22 define bean

--- spring-boot/src/main/java/com/zhong/define/bean/ImportResourceDemo.java
+++ spring-boot/src/main/java/com/zhong/define/bean/ImportResourceDemo.java
@@ -1,8 +1,6 @@
 package com.zhong.define.bean;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.stereotype.Component;
 
 /**
  * @author: juzi
@@ -10,7 +8,7 @@ import org.springframework.stereotype.Component;
  * @desc: 通过 @ImportResource 注解来引入一个 XML 配置文件中定义的 Bean，Spring会将其中定义的 Bean 纳入到容器管理中。
  */
 @Configuration
-@ImportResource("classpath:spring-config.xml")
+//@ImportResource("classpath:spring-config.xml")
 public class ImportResourceDemo {
 
 
