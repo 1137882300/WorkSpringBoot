ecc8f7115b845d96227dd443d171741bc52ea192
zzh <zzh@youxiake.com>
2023-06-27 11:14:27 +0800

    2023-6-27

---
+++ elastic-search/src/main/java/com/zhong/es/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * @author: juzi
+ * @date: 2023/6/27
+ * @desc:
+ */
+package com.zhong.es;
+
+/* es 所需依赖
+ *         <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-data-elasticsearch</artifactId>
+        </dependency>
+ */
---
+++ redis-cache/src/main/java/com/zhong/redis/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * @author: juzi
+ * @date: 2023/6/27
+ * @desc:
+ */
+package com.zhong.redis;
+
+/* 集成redis的所需依赖
+ *         <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-data-redis</artifactId>
+        </dependency>
+        * 下面这个可以不需要
+        <dependency>
+            <groupId>org.springframework.data</groupId>
+            <artifactId>spring-data-commons</artifactId>
+        </dependency>
+ */
